4f25a0908b9028c4226f1335330e1008e08d8a4f
BadrChoubai <badr.choubai1048@gmail.com>
2018-10-03 16:12:21 -0600

    Add Goal to graphql

--- imports/startup/server/register-api.js
+++ imports/startup/server/register-api.js
@@ -5,16 +5,19 @@ import merge from "lodash/merge";
 // Schemas
 import ResolutionsSchema from "../../api/resolutions/resolution.graphql";
 import UsersSchema from '../../api/users/User.graphql';
+import GoalsSchema from '../../api/goals/Goal.graphql'
 // Resolvers
 import ResolutionsResolvers from "../../api/resolutions/resolvers";
 import UsersResolvers from '../../api/users/resolvers';
+import GoalsResolver from '../../api/goals/resolvers'
 
-// jkl
-const typeDefs = [ResolutionsSchema, UsersSchema];
+// 
+const typeDefs = [ResolutionsSchema, UsersSchema, GoalsSchema];
 
 const resolvers = merge( 
   ResolutionsResolvers, 
-  UsersResolvers
+  UsersResolvers,
+  GoalsResolver
 );
 
 const schema = makeExecutableSchema({
